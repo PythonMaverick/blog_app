61a980b53f84245cafa27272c1766c2df13ec737
Marat Movlamov <marat.movlamov@moonbug.com>
2022-10-25 22:39:17 +0300

    Class Based view added for posts

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     # post views for the application level settings
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>',
          views.post_detail, name='post_detail'),
 ]
