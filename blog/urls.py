0af3d7382613b4d35b3f4d16c3d585d85b6dd5d6
Marat Movlamov <marat.movlamov@moonbug.com>
2022-10-29 01:14:50 +0300

    Email Settings and Templates

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,12 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    # post views for the application level settings
+    # Post views
     # path('', views.post_list, name='post_list'),
-    path('', views.PostListView, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>',
-         views.post_detail, name='post_detail'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail,
+         name='post_detail'),
+    path('<int:post_id>/share/',
+         views.post_share, name='post_share'),
 ]
