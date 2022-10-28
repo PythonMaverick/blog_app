f081f2ae85a4bcb65ee4fab92682b06bd3d83746
Marat Movlamov <marat.movlamov@moonbug.com>
2022-10-29 01:09:58 +0300

    Email SMTP added

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import EmailPostForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
@@ -42,3 +43,14 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 5
     template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.status.PUBLISHED)
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
