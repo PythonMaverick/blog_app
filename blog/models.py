5731409df6ee624e62bd52b7a7ee0decbf5cf281
Marat Movlamov <marat.movlamov@moonbug.com>
2022-10-31 00:47:07 +0300

    Tagging functionality added.

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -31,6 +32,9 @@ class Post(models.Model):
     objects = models.Manager()
     # New Custom Manager
     published = PublishedManager()
+
+    # Tag Manager for posts
+    tags = TaggableManager()
 
     class Meta:
         ordering = ['-publish']
