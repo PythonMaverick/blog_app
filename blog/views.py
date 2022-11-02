53aa033e0a6205f397ed3b91e348a9a041963572
Marat Movlamov <marat.movlamov@moonbug.com>
2022-11-02 23:55:45 +0300

    Fina;

--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,15 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     # Pagination with 5 posts per page
     paginator = Paginator(post_list, 5)
     page_number = request.GET.get('page', 1)
